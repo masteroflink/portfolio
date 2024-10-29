38304418fa94c131fff9c8ef99c5bd89246806fd
Bruce Bruno <bruce@Bruces-MBP.attlocal.net>
2024-10-29 17:08:07 -0400

    update doc titlle

--- src/App.js
+++ src/App.js
@@ -5,8 +5,12 @@ import About from "./components/About";
 import Skills from "./components/Skills";
 import Projects from "./components/Projects";
 import Footer from "./components/Footer";
+import { useEffect } from "react";
 
 const App = () => {
+  useEffect(() => {
+    document.title = "Bruce Bruno";
+  }, []);
   return (
     <div className="App">
       <Navbar />
