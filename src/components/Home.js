f686499043196073410b48cd094e6bbaff04a252
Bruce Bruno <bruce@Bruces-MBP.attlocal.net>
2024-10-29 12:19:11 -0400

    update intro sentence

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,7 +4,8 @@ const Home = () => {
   return (
     <div id="home" className="Home-main">
       <span className="intro">
-        Welcome, I am Bruce I am a Full-Stack engineer.
+        Welcome, I am a full-stack and data engineer skilled in developing
+        robust applications and managing data-driven solutions.
       </span>
     </div>
   );
