20a25b0c0bea8a0158127c55c1f0a4b8b99903f3
Bruce Bruno <bruce@Bruces-MBP.attlocal.net>
2024-10-29 12:10:22 -0400

    fix warning

--- src/components/FadeInOnScroll.js
+++ src/components/FadeInOnScroll.js
@@ -6,6 +6,7 @@ const FadeInOnScroll = ({ children }) => {
   const ref = useRef(null);
 
   useEffect(() => {
+    const currentRef = ref.current;
     const observer = new IntersectionObserver(
       ([entry]) => {
         if (entry.isIntersecting) {
@@ -17,13 +18,13 @@ const FadeInOnScroll = ({ children }) => {
       { threshold: 0.1 } // Adjust the threshold as needed
     );
 
-    if (ref.current) {
-      observer.observe(ref.current);
+    if (currentRef) {
+      observer.observe(currentRef);
     }
 
     return () => {
-      if (ref.current) {
-        observer.unobserve(ref.current);
+      if (currentRef) {
+        observer.unobserve(currentRef);
       }
     };
   }, []);
