dcbeb49feab146c9e773ed18a3b6508b96003be0
Bruce Bruno <bruce@Bruces-MBP.attlocal.net>
2024-10-29 11:55:58 -0400

    add animations and update skills list

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,3 +1,4 @@
+import FadeInOnScroll from "./FadeInOnScroll";
 import ProjectContainer from "./ProjectContainer";
 import "./Projects.css";
 import SectionTitle from "./SectionTitle";
@@ -12,7 +13,7 @@ const Projects = () => {
       imgSrc: `${process.env.PUBLIC_URL}/note_taker.jpg`,
       skills: ["Python", "HTML", "Django", "PostgreSQL"],
       description:
-        "Lorem ipsum odor amet, consectetuer adipiscing elit. Libero ad maecenas dis tellus maecenas per fringilla massa euismod.",
+        "The Note Taker is a web application developed using Python's Django framework, leveraging PostgreSQL for efficient data management. It allows users to create, edit, and organize notes seamlessly, featuring user authentication and a responsive design for accessibility across devices. This project demonstrates expertise in Django development, database integration, and creating a user-friendly interface for effective note management.",
     },
     {
       name: "Shopping Cart",
@@ -20,7 +21,7 @@ const Projects = () => {
       imgSrc: `${process.env.PUBLIC_URL}/shopping_cart.jpg`,
       skills: ["Go"],
       description:
-        "Lorem ipsum odor amet, consectetuer adipiscing elit. Libero ad maecenas dis tellus maecenas per fringilla massa euismod.",
+        "The Shopping Cart CLI Application is a command-line tool developed in Go that enables users to manage their shopping cart efficiently. It features functionalities for adding, removing, and viewing items, as well as calculating the total cost, all through an intuitive interface. This project showcases proficiency in Go programming and demonstrates the ability to create effective, user-friendly applications for managing e-commerce tasks.",
     },
     {
       name: "Todo List",
@@ -28,25 +29,27 @@ const Projects = () => {
       imgSrc: `${process.env.PUBLIC_URL}/todo_list.jpg`,
       skills: ["Java", "PostgreSQL", "Docker", "API"],
       description:
-        "Lorem ipsum odor amet, consectetuer adipiscing elit. Libero ad maecenas dis tellus maecenas per fringilla massa euismod.",
+        "The Todo List Backend API is a Java-based application that provides a RESTful interface for managing to-do items, utilizing PostgreSQL for data storage. Deployed in a Docker container, this project ensures scalability and easy integration into various environments. This implementation demonstrates expertise in Java development, API design, and containerization for efficient application deployment.",
     },
   ];
 
   return (
     <div id="projects" className="projects-main">
-      <SectionTitle title="Projects" />
-      <div className="projects-content">
-        {projectData.map((item) => (
-          <ProjectContainer
-            key={item.name}
-            projectName={item.name}
-            url={item.url}
-            imgSrc={item.imgSrc}
-            skills={item.skills}
-            description={item.description}
-          />
-        ))}
-      </div>
+      <FadeInOnScroll>
+        <SectionTitle title="Projects" />
+        <div className="projects-content">
+          {projectData.map((item) => (
+            <ProjectContainer
+              key={item.name}
+              projectName={item.name}
+              url={item.url}
+              imgSrc={item.imgSrc}
+              skills={item.skills}
+              description={item.description}
+            />
+          ))}
+        </div>
+      </FadeInOnScroll>
     </div>
   );
 };
--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,9 +1,11 @@
+import FadeInOnScroll from "./FadeInOnScroll";
 import SectionTitle from "./SectionTitle";
 import "./Skills.css";
 
 const Skills = () => {
   const skillsData = {
-    languages: ["Python", "Go", "Java", "Elixir"],
+    languages: ["Python", "Typescript", "Go", "Java", "Elixir"],
+    frameworks: ["Django", "React", "FastAPI"],
     tools: [
       "Google Cloud",
       "Azure",
@@ -13,42 +15,42 @@ const Skills = () => {
       "Cloud Functions",
       "PubSub",
     ],
+    databases: ["Big Query", "PostgreSQL", "MongoDB", "ElasticSearch", "Redis"],
+  };
+
+  const capitalize = (string) => {
+    if (!string) return ""; // Handle empty strings or undefined input
+    return string.charAt(0).toUpperCase() + string.slice(1).toLowerCase();
   };
 
   return (
     <div id="skills" name="skills-section" className="skills-main">
-      <SectionTitle title="Skills" />
-      <div className="skills-content">
-        <div className="skills-box">
-          <div>
-            <div className="skills-section">
-              <h3 className="skills-title">Language/Frameworks</h3>
-              <div className="skills-tags">
-                {skillsData["languages"].map((item) => (
-                  <span key={item} className="skills-tag">
-                    {item}
-                  </span>
-                ))}
-              </div>
-            </div>
-            <div className="skills-section">
-              <h3 className="skills-title">Tools</h3>
-              <div className="skills-tags">
-                {skillsData["tools"].map((item) => (
-                  <span key={item} className="skills-tag">
-                    {item}
-                  </span>
-                ))}
-              </div>
+      <FadeInOnScroll>
+        <SectionTitle title="Skills" />
+        <div className="skills-content">
+          <div className="skills-box">
+            <div>
+              {Object.keys(skillsData).map((item) => (
+                <div className="skills-section">
+                  <h3 className="skills-title">{capitalize(item)}</h3>
+                  <div className="skills-tags">
+                    {skillsData[item].map((skill) => (
+                      <span key={skill} className="skills-tag">
+                        {skill}
+                      </span>
+                    ))}
+                  </div>
+                </div>
+              ))}
             </div>
           </div>
+          <img
+            className="skills-img"
+            src={`${process.env.PUBLIC_URL}/skills.png`}
+            alt="https://cdn.pixabay.com/photo/2019/02/28/17/31/tasks-4026398_1280.jpg"
+          />
         </div>
-        <img
-          className="skills-img"
-          src={`${process.env.PUBLIC_URL}/skills.png`}
-          alt="https://cdn.pixabay.com/photo/2019/02/28/17/31/tasks-4026398_1280.jpg"
-        />
-      </div>
+      </FadeInOnScroll>
     </div>
   );
 };
